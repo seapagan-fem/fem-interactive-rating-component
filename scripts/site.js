f2bfcab6cdfa059ee870e3c8824ff76390affa92
Grant Ramsay <seapagan@gmail.com>
2022-07-08 12:19:13 +0100

    add js to select the rating and hilight button
    also remove hilight from previous selection

--- scripts/site.js
+++ scripts/site.js
@@ -1,20 +1,33 @@
 console.log("JavaScript loaded.");
 
+// this will store the current selected rating
 let choice = 0;
+
+const beforeEl = document.getElementById("before");
+const afterEl = document.getElementById("after");
 
 // get all the rating buttons into an HTMLCollection
 const ratingButtons = document.getElementsByClassName("rating-button");
 
-// this function will be called on each click of a rating button
+// this function will be called on each click of a rating button.
 const ratingClick = (e) => {
-  console.log(`rating button ${e.target.id} was Clicked!`);
-  choice = Number(e.target.id);
-  console.log(choice);
+  let newChoice = Number(e.target.id);
+  if (newChoice !== choice) {
+    choice !== 0
+      ? document
+          .getElementById(String(choice))
+          .classList.toggle("rating-selected")
+      : null;
+    choice = newChoice;
+    e.target.classList.toggle("rating-selected");
+  }
 };
 
 // handle clicking the 'submit' button
 const submitClick = (e) => {
   console.log(`Submit has been clicked and choice is ${choice}`);
+  beforeEl.classList.add("hide-me");
+  afterEl.classList.remove("hide-me");
 };
 
 // set a click handler for each of the rating buttons.
