32ac3530a169e2481d9a29d2961ee3b14eb0fb90
Grant Ramsay <seapagan@gmail.com>
2022-07-08 12:53:38 +0100

    implement first draft of the 'thanks' page
    with chosen vote displayed

--- scripts/site.js
+++ scripts/site.js
@@ -9,7 +9,7 @@ const afterEl = document.getElementById("after");
 // get all the rating buttons into an HTMLCollection
 const ratingButtons = document.getElementsByClassName("rating-button");
 
-// this function will be called on each click of a rating button.
+// handle clicking a rating button.
 const ratingClick = (e) => {
   let newChoice = Number(e.target.id);
   if (newChoice !== choice) {
@@ -25,9 +25,13 @@ const ratingClick = (e) => {
 
 // handle clicking the 'submit' button
 const submitClick = (e) => {
-  console.log(`Submit has been clicked and choice is ${choice}`);
-  beforeEl.classList.add("hide-me");
-  afterEl.classList.remove("hide-me");
+  if (choice !== 0) {
+    // flip to the second state
+    beforeEl.classList.add("hide-me");
+    afterEl.classList.remove("hide-me");
+    // inject the chosen rating into the text
+    document.getElementById("rating-value").innerHTML = choice;
+  }
 };
 
 // set a click handler for each of the rating buttons.
